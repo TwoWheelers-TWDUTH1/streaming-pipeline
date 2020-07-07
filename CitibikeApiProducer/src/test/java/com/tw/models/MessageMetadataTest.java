2e276eb9981f3a42dd9a9688c9fa2dcae09103f3
tobias vogel <tvogel@thoughtworks.com>
2020-07-07 18:18:07 +0700

    tobi - use heavyweight Java8 dateTime API instead of assertJ stuff

--- CitibikeApiProducer/src/test/java/com/tw/models/MessageMetadataTest.java
+++ CitibikeApiProducer/src/test/java/com/tw/models/MessageMetadataTest.java
@@ -1,7 +1,10 @@
 package com.tw.models;
 
-import org.assertj.core.util.DateUtil;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,7 +12,8 @@ public class MessageMetadataTest {
 
     @Test
     public void shouldReturnStringWithMetadataAndPayload() {
-        long ingestionTime = 1483246800000l;
+        ZonedDateTime zonedDateTime = LocalDateTime.parse("2017-01-01T00:00:00").atZone(TimeZone.getTimeZone("Bangkok/Thailand").toZoneId());
+        long ingestionTime = zonedDateTime.toInstant().toEpochMilli();
         String producerId = "Producer-id";
         String messageUUID = "123e4567-e89b-12d3-a456-426655440000";
         long size = 12;
@@ -19,7 +23,7 @@ public class MessageMetadataTest {
         String expected = "{\"producer_id\": \"Producer-id\", " +
                 "\"size\": 12, " +
                 "\"message_id\": \"123e4567-e89b-12d3-a456-426655440000\", " +
-                "\"ingestion_time\": 1483246800000}";
+                "\"ingestion_time\": 1483228800000}";
 
         assertEquals(expected, metadata.toString());
     }
