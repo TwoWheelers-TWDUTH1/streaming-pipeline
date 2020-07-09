4ee22f1b0ab0cb8bb9ee7cc00691dea6c744cce4
leprechaun <leprechaun@gmail.com>
2020-07-09 18:41:44 +0700

    SmokeTest: refactor job and tests

--- SmokeTest/src/main/scala/com/tw/apps/SmokeTest.scala
+++ SmokeTest/src/main/scala/com/tw/apps/SmokeTest.scala
@@ -9,67 +9,111 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object SmokeTest {
 
   def assertThatLatitudeAndLongitudeAreNeverNull(df: sql.DataFrame) = {
-    val nullLongitudeCount = df.where("longitude is null").count()
-    assert(nullLongitudeCount == 0, "longitude has null values")
+    var probes : List[(String, Boolean, Int, StandardUnit)] = List()
 
-    val nullLatitudeCount = df.where("latitude is null").count()
-    assert(nullLatitudeCount == 0, "latitude has null values")
+    val nullLongitudeCount = df.where("longitude is null").count().toInt
+    probes = probes:+(("null-longitude", nullLongitudeCount > 0, nullLongitudeCount, StandardUnit.Count))
 
-    true
+    val nullLatitudeCount = df.where("latitude is null").count().toInt
+    probes = probes:+(("null-latitude", nullLatitudeCount > 0, nullLatitudeCount, StandardUnit.Count))
+
+    probes
   }
 
-  def assertThatNoCountsAreNegative(df: sql.DataFrame): Unit = {
+  def assertThatNoCountsAreNegative(df: sql.DataFrame) = {
+    var probes : List[(String, Boolean, Int, StandardUnit)] = List()
     val stationsWithNegativeCount = df.
-        where("bikes_available < 0 OR docks_available < 0")
+        where("bikes_available < 0 OR docks_available < 0").count().toInt
 
-    assert(stationsWithNegativeCount.count() == 0, "There are stations with negative counts (!)")
-
-    true
+    probes = (probes:+(("negative-counts", stationsWithNegativeCount > 0, stationsWithNegativeCount, StandardUnit.Count)))
+    probes
   }
 
-  def assertThatUpdatesAreRecent(df: DataFrame, cw: AmazonCloudWatch, currentTimeInSeconds: Long): Unit = {
+  def assertThatUpdatesAreRecent(df: DataFrame, currentTimeInSeconds: Long) = {
+    var probes : List[(String, Boolean, Int, StandardUnit)] = List()
+
     val medianLastUpdateUnixTimestamp = df.stat.approxQuantile("last_updated", Array(0.5), 0.25)(0)
     val currentUnixTimestamp = currentTimeInSeconds
 
     val diffInSeconds = currentUnixTimestamp - medianLastUpdateUnixTimestamp
 
+    probes = probes:+(("station-last-updated-age", diffInSeconds > 600, diffInSeconds.toInt, StandardUnit.Seconds))
+
+    probes
+  }
+
+  def assertThatLatitudeAndLongitudeAreCorrectlyTyped(df: sql.DataFrame) = {
+    var mistypedFields = 0
+    var misnamedFields = 0
+    var probes : List[(String, Boolean, Int, StandardUnit)] = List()
+
+    if(df.schema.fields(7).name != "latitude") {
+      misnamedFields = misnamedFields + 1
+    }
+
+    if(df.schema.fields(7).dataType.typeName != "double") {
+      mistypedFields = mistypedFields + 1
+    }
+
+    if(df.schema.fields(8).name != "longitude") {
+      misnamedFields = misnamedFields + 1
+    }
+
+    if(df.schema.fields(8).dataType.typeName != "double") {
+      mistypedFields = mistypedFields + 1
+    }
+
+    probes = probes:+(("misnamed-fields", misnamedFields > 0, misnamedFields, StandardUnit.Count))
+    probes = probes:+(("mistyped-fields", mistypedFields > 0, mistypedFields, StandardUnit.Count))
+
+    //assert(df.schema.fields(7).name == "latitude", "field isn't called `latitude`")
+    //assert(df.schema.fields(7).dataType.typeName == "double", "field `latitude` is not a double")
+    //assert(df.schema.fields(8).name == "longitude", "field isn't called `longitude`")
+    //assert(df.schema.fields(8).dataType.typeName == "double", "field `longitude` is not a double")
+
+    probes
+  }
+
+  def assertThatStationIdsAreUnique(df: sql.DataFrame) = {
+    var probes: List[(String, Boolean, Int, StandardUnit)] = List()
+
+    val stationIdsWithMoreThanOneRow = df.select("station_id").groupBy("station_id").agg(count("station_id") as "count").where("count > 1").count().toInt
+    probes = probes:+(("duplicated-station-ids", stationIdsWithMoreThanOneRow > 0, stationIdsWithMoreThanOneRow, StandardUnit.Count))
+    probes
+  }
+
+  def runAssertions(df: DataFrame, cw: AmazonCloudWatch, currentTimeInSeconds: Long) = {
+
+    val probes = List.concat(
+      assertThatStationIdsAreUnique(df),
+      assertThatLatitudeAndLongitudeAreCorrectlyTyped(df),
+      assertThatLatitudeAndLongitudeAreNeverNull(df),
+      assertThatNoCountsAreNegative(df),
+      assertThatUpdatesAreRecent(df, currentTimeInSeconds)
+    )
+
+    publishMetrics(cw, probes)
+
+    probes
+  }
+
+  def publishMetrics(cw: AmazonCloudWatch, probes: List[(String, Boolean, Int, StandardUnit)]) = {
+    probes.map( probe => {
+      publishMetric(cw, probe._1, probe._3, probe._4)
+    })
+  }
+
+  def publishMetric(cw: AmazonCloudWatch, metric : String, value: Int, unit: StandardUnit): Unit = {
     val datum = new MetricDatum()
-      .withMetricName("station-last-updated-age")
-      .withUnit(StandardUnit.Seconds)
-      .withValue(diffInSeconds)
+      .withMetricName(metric)
+      .withUnit(unit)
+      .withValue(value.toDouble)
 
     val request = new PutMetricDataRequest()
       .withNamespace("stationMart-monitoring")
       .withMetricData(datum)
 
     cw.putMetricData(request)
-
-    assert(diffInSeconds < 10 * 60, s"Median station update age is ${diffInSeconds}s but was supposed to be less than 600s")
-  }
-
-
-  def runAssertions(df: DataFrame, cw: AmazonCloudWatch, currentTimeInSeconds: Long) = {
-    assertThatStationIdsAreUnique(df)
-    assertThatLatitudeAndLongitudeAreCorrectlyTyped(df)
-    assertThatLatitudeAndLongitudeAreNeverNull(df)
-    assertThatNoCountsAreNegative(df)
-    assertThatUpdatesAreRecent(df, cw, currentTimeInSeconds)
-    true
-  }
-
-  def assertThatLatitudeAndLongitudeAreCorrectlyTyped(df: sql.DataFrame) = {
-    assert(df.schema.fields(7).name == "latitude", "field isn't called `latitude`")
-    assert(df.schema.fields(7).dataType.typeName == "double", "field `latitude` is not a double")
-    assert(df.schema.fields(8).name == "longitude", "field isn't called `longitude`")
-    assert(df.schema.fields(8).dataType.typeName == "double", "field `longitude` is not a double")
-
-    true
-  }
-
-  def assertThatStationIdsAreUnique(df: sql.DataFrame) = {
-    val stationsIdsWithMoreThanOneRow = df.select("station_id").groupBy("station_id").agg(count("station_id") as "count").where("count > 1")
-    assert(stationsIdsWithMoreThanOneRow.count() == 0, "there are duplicate stations")
-    true
   }
 
   def main(args: Array[String]): Unit = {
--- SmokeTest/src/test/scala/com/tw/apps/SmokeTestTest.scala
+++ SmokeTest/src/test/scala/com/tw/apps/SmokeTestTest.scala
@@ -2,103 +2,143 @@ package com.tw.apps
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch
 import com.amazonaws.services.cloudwatch.model.{PutMetricDataRequest, StandardUnit}
+import org.apache.spark.sql
 import org.apache.spark.sql.SparkSession
 import org.mockito.{ArgumentCaptor, Mockito}
 import org.mockito.Mockito.verify
+import org.mockito.Mockito.times
 import org.scalatest._
 import org.scalatest.mockito.MockitoSugar
 
 class SmokeTestTest extends FeatureSpec with MockitoSugar with Matchers with GivenWhenThen with BeforeAndAfter {
-  feature("Apply station status transformations to data frame") {
-    val spark = SparkSession.builder.appName("Test App").master("local").getOrCreate()
-    val cloudWatchMock: AmazonCloudWatch = mock[AmazonCloudWatch]
+  val spark = SparkSession.builder.appName("Test App").master("local").getOrCreate()
+  val cloudWatchMock: AmazonCloudWatch = mock[AmazonCloudWatch]
 
-    before {
-      Mockito.reset(cloudWatchMock)
+  before {
+    Mockito.reset(cloudWatchMock)
+  }
+
+  def readCsv(path : String) = {
+    spark.read
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv(path)
+  }
+
+  def getProbes(df: sql.DataFrame) = {
+    SmokeTest.runAssertions(df, cloudWatchMock, 1594115095)
+  }
+
+  feature("sending metrics to CloudWatch") {
+    scenario("sends metrics for each probe") {
+      val OVER_NINE_THOUSAND = 9001
+      val THE_MEANING_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING = 42
+      val probes: List[(String, Boolean, Int, StandardUnit)] = List(
+        (
+          "some-probe-metric", true, OVER_NINE_THOUSAND, StandardUnit.Count
+        ),
+        (
+          "another-probe-metric", false, THE_MEANING_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING, StandardUnit.Count
+        )
+      )
+
+      SmokeTest.publishMetrics(cloudWatchMock, probes)
+
+      val requestCaptor : ArgumentCaptor[PutMetricDataRequest] = ArgumentCaptor.forClass(classOf[PutMetricDataRequest])
+      verify(cloudWatchMock, times(2)).putMetricData(requestCaptor.capture())
     }
 
-    scenario("passes and returns the latest `last_updated`") {
-      val validDF = spark.read
-        .option("inferSchema", "true")
-        .option("header", "true")
-        .csv("./src/test/resources/valid.csv")
-        .cache()
+    scenario("sends metrics with the right arguments") {
+      val OVER_NINE_THOUSAND = 9001
+      val probes: List[(String, Boolean, Int, StandardUnit)] = List(
+        (
+          "some-probe-metric", true, OVER_NINE_THOUSAND, StandardUnit.Count
+        )
+      )
 
-      SmokeTest.runAssertions(validDF, cloudWatchMock, 1594115095)
+      SmokeTest.publishMetrics(cloudWatchMock, probes)
+
+      val requestCaptor : ArgumentCaptor[PutMetricDataRequest] = ArgumentCaptor.forClass(classOf[PutMetricDataRequest])
+      verify(cloudWatchMock, times(1)).putMetricData(requestCaptor.capture())
+
+      val request = requestCaptor.getValue
+      println(request)
+
+      val metricDatum = request.getMetricData.get(0)
+      metricDatum.getValue shouldEqual(OVER_NINE_THOUSAND)
+      metricDatum.getMetricName shouldEqual("some-probe-metric")
+      metricDatum.getUnit shouldEqual("Count")
+    }
+  }
+
+  feature("Recognizes various successes and failures") {
+    scenario("passes and returns the latest `last_updated`") {
+      val validDF = readCsv("./src/test/resources/valid.csv")
+
+      val probes = getProbes(validDF)
+
+      val failureProbeCount = probes.count( probe => {
+        probe._2 == true
+      })
+
+      val ageProbe = probes.filter( probe => {
+        probe._1 == "station-last-updated-age"
+      })
+
+      failureProbeCount shouldEqual(0)
+      ageProbe(0)._3 shouldEqual(10)
+    }
+
+    scenario("recognises stale data") {
+      val validDF = readCsv("./src/test/resources/valid.csv")
+
+      val probes = SmokeTest.runAssertions(validDF, cloudWatchMock, 1594125095)
+
+      val failureProbeCount = probes.count( probe => {
+        probe._1 == "station-last-updated-age" && probe._2
+      })
+
+      failureProbeCount shouldEqual(1)
     }
 
     scenario("explodes when there are null longitudes") {
-      val validDF = spark.read
-        .option("inferSchema", "true")
-        .option("header", "true")
-        .csv("./src/test/resources/has-null-longitude.csv")
-        .cache()
+      val invalidDF = readCsv("./src/test/resources/has-null-longitude.csv")
+      val probes = getProbes(invalidDF)
 
-      val exception = intercept[AssertionError] {
-        SmokeTest.runAssertions(validDF, cloudWatchMock, 1594115095)
-      }
-      assert(exception.getMessage.contains("longitude has null values"))
+      probes.count(probe => { probe._2 }) shouldEqual(1)
+      probes.count( probe => {
+        probe._2 == true && probe._1 == "null-longitude"
+      }) shouldEqual(1)
     }
 
     scenario("explodes when there are null latitudes") {
-      val validDF = spark.read
-        .option("inferSchema", "true")
-        .option("header", "true")
-        .csv("./src/test/resources/has-null-latitude.csv")
-        .cache()
+      val invalidDF = readCsv("./src/test/resources/has-null-latitude.csv")
+      val probes = getProbes(invalidDF)
 
-      val exception = intercept[AssertionError] {
-        SmokeTest.runAssertions(validDF, cloudWatchMock, 1594115095)
-      }
-
-      assert(exception.getMessage.contains("latitude has null values"))
+      probes.count(probe => { probe._2 }) shouldEqual(1)
+      probes.count( probe => {
+        probe._2 == true && probe._1 == "null-latitude"
+      }) shouldEqual(1)
     }
 
     scenario("explodes when there are duplicated station_ids") {
-      val validDF = spark.read
-        .option("inferSchema", "true")
-        .option("header", "true")
-        .csv("./src/test/resources/has-duplicated-station-ids.csv")
-        .cache()
+      val invalidDF = readCsv("./src/test/resources/has-duplicated-station-ids.csv")
+      val probes = getProbes(invalidDF)
 
-      val exception = intercept[AssertionError] {
-        SmokeTest.runAssertions(validDF, cloudWatchMock, 1594115095)
-      }
-
-      assert(exception.getMessage.contains("there are duplicate stations"))
+      probes.count(probe => { probe._2 }) shouldEqual(1)
+      probes.count( probe => {
+        probe._2 == true && probe._1 == "duplicated-station-ids"
+      }) shouldEqual(1)
     }
 
     scenario("explodes when there are negative counts") {
-      val validDF = spark.read
-        .option("inferSchema", "true")
-        .option("header", "true")
-        .csv("./src/test/resources/has-negative-counts.csv")
-        .cache()
+      val invalidDF = readCsv("./src/test/resources/has-negative-counts.csv")
+      val probes = getProbes(invalidDF)
 
-      val exception = intercept[AssertionError] {
-        SmokeTest.runAssertions(validDF, cloudWatchMock, 1594115095)
-      }
-
-      assert(exception.getMessage.contains("negative counts"))
-    }
-
-    scenario("reports the median age of station updates") {
-      val validDF = spark.read
-        .option("inferSchema", "true")
-        .option("header", "true")
-        .csv("./src/test/resources/valid.csv")
-        .cache()
-
-      SmokeTest.runAssertions(validDF, cloudWatchMock, 1594115095)
-      val requestCaptor : ArgumentCaptor[PutMetricDataRequest] = ArgumentCaptor.forClass(classOf[PutMetricDataRequest])
-
-      verify(cloudWatchMock).putMetricData(requestCaptor.capture())
-      val request = requestCaptor.getValue
-      val metricDatum = request.getMetricData.get(0)
-
-      metricDatum.getValue shouldEqual(10.0)
-      metricDatum.getUnit shouldEqual(StandardUnit.Seconds.toString)
-      request.getNamespace shouldEqual("stationMart-monitoring")
+      probes.count(probe => { probe._2 }) shouldEqual(1)
+      probes.count( probe => {
+        probe._2 == true && probe._1 == "negative-counts"
+      }) shouldEqual(1)
     }
   }
 }
