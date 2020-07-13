bd8223204b94f8d5514a2f078963df45cc45bd72
pitchayut-junior-tw <pitchayut.c@thoughtworks.com>
2020-07-13 17:46:44 +0700

    [Jr] change metric name to is_app_running & add jobFlowId

--- RawDataSaver/src/main/scala/com/tw/apps/CloudWatchSparkListener.scala
+++ RawDataSaver/src/main/scala/com/tw/apps/CloudWatchSparkListener.scala
@@ -4,59 +4,49 @@ package com.tw.apps
 
 import java.util
 
-import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder
+import com.amazonaws.services.cloudwatch.{AmazonCloudWatch, AmazonCloudWatchClientBuilder}
 import com.amazonaws.services.cloudwatch.model.{Dimension, MetricDatum, PutMetricDataRequest, StandardUnit}
 import org.apache.log4j.Logger
 import org.apache.spark.sql.streaming.StreamingQueryListener
 import org.apache.spark.sql.streaming.StreamingQueryListener.{QueryProgressEvent, QueryStartedEvent, QueryTerminatedEvent}
-import org.apache.spark.streaming.scheduler.{StreamingListener, _}
 
 import scala.collection.mutable.{HashMap, Map}
+import scala.io.{BufferedSource, Source}
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 
 class CloudWatchSparkListener(appName: String = "ApplicationName") extends StreamingQueryListener {
 
-  val log = Logger.getLogger(getClass.getName)
+  val log: Logger = Logger.getLogger(getClass.getName)
   val dimensionsMap = new HashMap[String, String]()
-  val cw = AmazonCloudWatchClientBuilder.defaultClient()
-
-//  override def onBatchCompleted(batchCompleted: StreamingListenerBatchCompleted): Unit = {
-//    log.info("CloudWatch Streaming Listener, onBatchCompleted:" + appName)
-//
-//    // write performance metrics to CloutWatch Metrics
-//    writeBatchStatsToCloudWatch(batchCompleted)
-//
-//  }
-//
-//  override def onReceiverError(receiverError: StreamingListenerReceiverError): Unit = {
-//    log.warn("CloudWatch Streaming Listener, onReceiverError:" + appName)
-//
-//    writeRecieverStatsToCloudWatch(receiverError)
-//  }
+  val cw: AmazonCloudWatch = AmazonCloudWatchClientBuilder.defaultClient()
+  val jobFlowInfoFile = "/mnt/var/lib/info/job-flow.json"
+  val jobFlowId: String = getJobFlowId()
 
   override def onQueryStarted(event: QueryStartedEvent): Unit = {
     log.info("Cloudwatch Streaming Listener, onQueryStarted: " + appName)
-    pushMetric(dimensionsMap, "started", 1.0, StandardUnit.Count)
+    pushMetric(dimensionsMap, "is_app_running", 1, StandardUnit.Count)
   }
 
   override def onQueryProgress(event: QueryProgressEvent): Unit = {
     log.info("Cloudwatch Streaming Listener, onQueryProgress: " + appName)
-    pushMetric(dimensionsMap, "progress", 1.0, StandardUnit.Count)
+    pushMetric(dimensionsMap, "is_app_running", 1, StandardUnit.Count)
   }
 
   override def onQueryTerminated(event: QueryTerminatedEvent): Unit = {
     log.info("Cloudwatch Streaming Listener, onQueryTerminated: " + appName)
-    pushMetric(dimensionsMap, "terminated", 1.0, StandardUnit.Count)
+    pushMetric(dimensionsMap, "is_app_running", 0, StandardUnit.Count)
   }
 
-//
-//  def writeRecieverStatsToCloudWatch(receiverError: StreamingListenerReceiverError): Unit = {
-//
-//    sendHeartBeat(dimensionsMap, "receiverError")
-//
-//  }
+  def parseJsonWithJackson(json: BufferedSource): Map[String, Object] = {
+    val attrMapper = new ObjectMapper() with ScalaObjectMapper
+    attrMapper.registerModule(DefaultScalaModule)
+    attrMapper.readValue[Map[String, Object]](json.reader())
+  }
 
-  def sendHeartBeat(dimensionItems: Map[String, String], metricName: String): Unit = {
-    pushMetric(dimensionItems, "heartBeat", 1.0, StandardUnit.Count)
+  def getJobFlowId: String = {
+    parseJsonWithJackson(Source.fromFile(jobFlowInfoFile)).get("jobFlowId").mkString
   }
 
   def pushMetric(dimensionItems: Map[String, String], metricName: String, value: Double, unit: StandardUnit) {
@@ -75,7 +65,7 @@ class CloudWatchSparkListener(appName: String = "ApplicationName") extends Strea
 
     var dimentionsJobFlowId = new Dimension()
       .withName("JobFlowId")
-      .withValue("not sure yet")
+      .withValue(jobFlowId)
 
     dimensions.add(dimentionsJobFlowId)
 
@@ -94,45 +84,5 @@ class CloudWatchSparkListener(appName: String = "ApplicationName") extends Strea
       log.warn("Failed pushing CloudWatch Metric with RequestId: " + response.getSdkResponseMetadata.getRequestId)
       log.debug("Response Status code: " + response.getSdkHttpMetadata.getHttpStatusCode)
     }
-  }
-
-  def writeBatchStatsToCloudWatch(batchCompleted: StreamingListenerBatchCompleted): Unit = {
-
-    val processingTime = if (batchCompleted.batchInfo.processingDelay.isDefined) {
-      batchCompleted.batchInfo.processingDelay.get
-    }
-
-    val schedulingDelay = if (batchCompleted.batchInfo.schedulingDelay.isDefined && batchCompleted.batchInfo.schedulingDelay.get > 0) {
-      batchCompleted.batchInfo.schedulingDelay.get
-    } else {
-      0
-    }
-
-    val numRecords = batchCompleted.batchInfo.numRecords
-    sendHeartBeat(dimensionsMap, "batchCompleted")
-    pushMillisecondsMetric(dimensionsMap, "schedulingDelay", schedulingDelay)
-    pushMillisecondsMetric(dimensionsMap, "processingDelay", batchCompleted.batchInfo.processingDelay.get)
-    pushCountMetric(dimensionsMap, "numRecords", numRecords)
-    pushMillisecondsMetric(dimensionsMap, "totalDelay", batchCompleted.batchInfo.totalDelay.get);
-
-    log.info("Batch completed at: " + batchCompleted.batchInfo.processingEndTime.get +
-      " was started at: " + batchCompleted.batchInfo.processingStartTime.get +
-      " submission time: " + batchCompleted.batchInfo.submissionTime +
-      " batch time: " + batchCompleted.batchInfo.batchTime +
-      " batch processing delay: " + batchCompleted.batchInfo.processingDelay.get +
-      " records : " + numRecords +
-      " total batch delay:" + batchCompleted.batchInfo.totalDelay.get +
-      " product prefix:" + batchCompleted.batchInfo.productPrefix +
-      " schedulingDelay:" + schedulingDelay +
-      " processingTime:" + processingTime
-    )
-  }
-
-  def pushCountMetric(dimentionItems: Map[String, String], metricName: String, value: Double) {
-    pushMetric(dimentionItems, metricName, value.doubleValue(), StandardUnit.Count);
-  }
-
-  def pushMillisecondsMetric(dimentionItems: Map[String, String], metricName: String, value: Long) {
-    pushMetric(dimentionItems, metricName, value.longValue(), StandardUnit.Milliseconds);
   }
 }
