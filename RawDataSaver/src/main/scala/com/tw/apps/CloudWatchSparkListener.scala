ff92068d727219045c25d6dba9270cbd78377573
tobias vogel <tvogel@thoughtworks.com>
2020-07-15 14:21:12 +0700

    [tobi] add a few more metrics to the SparkListener

--- RawDataSaver/src/main/scala/com/tw/apps/CloudWatchSparkListener.scala
+++ RawDataSaver/src/main/scala/com/tw/apps/CloudWatchSparkListener.scala
@@ -25,7 +25,10 @@ class CloudWatchSparkListener(appName: String, jobFlowFilePath: String, cloudWat
   override def onQueryProgress(event: QueryProgressEvent): Unit = {
     log.info("Cloudwatch Streaming Listener, onQueryProgress: " + appName)
     pushMetric("is_app_running", 1, StandardUnit.Count)
-    pushMetric("progress_num_input_rows", event.progress.numInputRows, StandardUnit.Count)
+
+    if ( !event.progress.inputRowsPerSecond.isNaN ) pushMetric("inputRowsPerSecond", event.progress.inputRowsPerSecond, StandardUnit.Seconds)
+    if ( !event.progress.processedRowsPerSecond.isNaN ) pushMetric("processedRowsPerSecond", event.progress.processedRowsPerSecond, StandardUnit.Seconds)
+    pushMetric("numberOfInputRows", event.progress.numInputRows, StandardUnit.Count)
   }
 
   override def onQueryTerminated(event: QueryTerminatedEvent): Unit = {
