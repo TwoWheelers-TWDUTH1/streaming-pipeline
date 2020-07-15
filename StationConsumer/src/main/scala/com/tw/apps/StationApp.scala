7e60329230538b1683e418dc257a657bb66414cb
Satha Chaojaroenrat <sathachaojaroenrat@ip-192-168-1-103.ap-southeast-1.compute.internal>
2020-07-15 18:12:36 +0700

    Revert write format back to  and  mode

--- StationConsumer/src/main/scala/com/tw/apps/StationApp.scala
+++ StationConsumer/src/main/scala/com/tw/apps/StationApp.scala
@@ -66,7 +66,7 @@ object StationApp {
       .reduceGroups((r1,r2)=>if (r1.last_updated > r2.last_updated) r1 else r2)
       .map(_._2)
       .writeStream
-      .format("csv")
+      .format("overwriteCSV")
       .outputMode("complete")
       .option("header", true)
       .option("truncate", false)
