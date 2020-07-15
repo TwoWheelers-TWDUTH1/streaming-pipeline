9c43e7c2d162efbcc08ffcb77662f2e36d009ce4
Satha Chaojaroenrat <sathachaojaroenrat@ip-192-168-1-103.ap-southeast-1.compute.internal>
2020-07-15 16:55:58 +0700

    Try using append output mode instead of complete overwrite

--- StationConsumer/src/main/scala/com/tw/apps/StationApp.scala
+++ StationConsumer/src/main/scala/com/tw/apps/StationApp.scala
@@ -66,8 +66,8 @@ object StationApp {
       .reduceGroups((r1,r2)=>if (r1.last_updated > r2.last_updated) r1 else r2)
       .map(_._2)
       .writeStream
-      .format("overwriteCSV")
-      .outputMode("complete")
+      .format("csv")
+      .outputMode("append")
       .option("header", true)
       .option("truncate", false)
       .option("checkpointLocation", checkpointLocation)
