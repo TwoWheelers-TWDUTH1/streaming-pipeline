38198ee28cee9db68921169f698b02a276637925
Satha Chaojaroenrat <sathachaojaroenrat@ip-192-168-1-103.ap-southeast-1.compute.internal>
2020-07-17 15:33:18 +0700

    Add additional aggregation test to test writeStream

--- StationConsumer/src/test/scala/com/tw/apps/StationAggregationTest.scala
+++ StationConsumer/src/test/scala/com/tw/apps/StationAggregationTest.scala
@@ -1,6 +1,8 @@
 package com.tw.apps
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.catalyst.ScalaReflection
+import org.apache.spark.sql.types.StructType
 import org.scalatest.GivenWhenThen
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
@@ -29,7 +31,37 @@ class StationAggregationTest extends AnyFeatureSpec with Matchers with GivenWhen
       val expectedStationData = Array(
         StationData(11, 12, true, true, 1594882800, "0deb7e762d80f771360306ef132bce3d", "Valencia St at 16th St", 37.765052, -122.4218661)
       )
-      outputDS.collect shouldBe (expectedStationData)
+      outputDS.collect shouldBe expectedStationData
+    }
+
+    ignore("Write aggregated station data to data sink") {
+      Given("Sample station data")
+      val inputDataPath = getClass.getResource("/data").toURI
+      val schema = ScalaReflection.schemaFor[StationData].dataType.asInstanceOf[StructType]
+      val testStationDS = spark.readStream
+          .format("csv")
+          .schema(schema)
+          .option("header", true)
+          .option("path", inputDataPath.toString)
+          .option("includeTimestamp", true)
+          .load()
+          .as[StationData]
+
+      When("Most updated data is selected")
+      val outputDS = StationAggregation.selectMostUpdatedStationData(testStationDS, spark)
+
+      Then("Only most updated data remained")
+      val outputQuery = outputDS.writeStream
+        .format("memory")
+        .queryName("Output")
+        .outputMode("append")
+        .start()
+      outputQuery.processAllAvailable()
+      val results = spark.sql("select * from Output").collect
+      val expectedStationData = Array(
+        Row(11, 12, true, true, 1594882800, "0deb7e762d80f771360306ef132bce3d", "Valencia St at 16th St", 37.765052, -122.4218661)
+      )
+      results shouldBe (expectedStationData)
     }
 
   }
